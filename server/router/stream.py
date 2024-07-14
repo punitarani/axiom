d36e8436467ef77853281c3022f52acd6baa1467
Punit Arani <punitsai36@gmail.com>
2024-07-14 15:31:26 -0700

    Update stream router endpoints to use websocket

--- server/router/stream.py
+++ server/router/stream.py
@@ -3,47 +3,48 @@
 import asyncio
 import datetime
 import json
-import time
 
 import pytz
-from fastapi import APIRouter, HTTPException
-from fastapi.responses import StreamingResponse
+from fastapi import APIRouter, HTTPException, WebSocket, WebSocketDisconnect
 
 from axiom.store.cache import level_one_cache
 
 router = APIRouter(prefix="/stream")
 
 
-def generate_clock() -> str:
+@router.websocket("/clock")
+async def clock(websocket: WebSocket):
+    await websocket.accept()
     est = pytz.timezone("US/Eastern")
-    while True:
-        now = datetime.datetime.now(est).strftime("%Y-%m-%d %H:%M:%S")
-        yield json.dumps({"time": now})
-        time.sleep(1)
+    try:
+        while True:
+            now = datetime.datetime.now(est).strftime("%Y-%m-%d %H:%M:%S")
+            await websocket.send_text(json.dumps({"time": now}))
+            await asyncio.sleep(1)
+    except WebSocketDisconnect:
+        print("Client disconnected")
 
 
-@router.get("/clock")
-async def clock():
-    return StreamingResponse(generate_clock(), media_type="application/json")
-
-
-@router.get("/equity/level-one")
-async def equity_level_one(frequency: int = 10):
+@router.websocket("/equity/level-one")
+async def equity_level_one(websocket: WebSocket, frequency: int = 10):
     if frequency < 1 or frequency > 60:
-        raise HTTPException(status_code=400, detail="Frequency must be between 1 and 60 seconds")
+        await websocket.close(code=4000)
+        return
 
-    async def generate_equity_level_one():
-        key_error_count = 0
+    await websocket.accept()
+    key_error_count = 0
+    try:
         while True:
             try:
                 data = level_one_cache["SPY"]
-                yield data
+                await websocket.send_text(data)
                 key_error_count = 0  # Reset the count if data is found
                 await asyncio.sleep(frequency)
             except KeyError:
                 key_error_count += 1
                 if key_error_count >= 3:
-                    raise HTTPException(status_code=500, detail="Error fetching data")
+                    await websocket.close(code=5000)
+                    return
                 await asyncio.sleep(frequency)
-
-    return StreamingResponse(generate_equity_level_one(), media_type="application/json")
+    except WebSocketDisconnect:
+        print("Client disconnected")
