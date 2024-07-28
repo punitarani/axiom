755d3b173a3e67e39a9a612003e2193399cac950
Punit Arani <punitsai36@gmail.com>
2024-07-27 20:36:13 -0700

    Add auth_router to /server/router package

--- server/router/__init__.py
+++ server/router/__init__.py
@@ -4,6 +4,7 @@ server.router package
 FastAPI Server Routers
 """
 
+from .auth import router as auth_router
 from .equity import router as equity_router
 from .ml import router as ml_router
 from .stream import router as stream_router
