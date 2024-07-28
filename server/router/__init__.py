0b7bbe80714258038e7d2af720044df12961797e
Punit Arani <punitsai36@gmail.com>
2024-07-28 12:21:28 -0700

    Add __all__ to server.router package

--- server/router/__init__.py
+++ server/router/__init__.py
@@ -8,3 +8,5 @@ from .auth import router as auth_router
 from .equity import router as equity_router
 from .ml import router as ml_router
 from .stream import router as stream_router
+
+__all__ = ["auth_router", "equity_router", "ml_router", "stream_router"]
