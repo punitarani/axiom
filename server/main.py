41642a8cacbd032cbd94661e7602b80932b9c3c4
Punit Arani <punitsai36@gmail.com>
2024-07-28 14:12:44 -0700

    Revert "Create /start and /stop endpoints to start/stop the streams instead of auto-starting in the lifespan contextmanager"
    This reverts commit 9d01c581

--- server/main.py
+++ server/main.py
@@ -6,11 +6,12 @@ FastAPI app for axiom
 
 import asyncio
 import os
-from asyncio import Task
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
+from axiom.ws.equity_level_one import run_equity_level_one_stream
 
 from .load import download_schwab_token, load_models
 from .router import auth_router, equity_router, ml_router, stream_router
@@ -18,15 +19,16 @@ from .router import auth_router, equity_router, ml_router, stream_router
 MODE = os.getenv("MODE", "prod")
 PORT = int(os.getenv("PORT", 8123))
 
-stream_task: Task | None = None
-
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):  # noqa (allow app to be unused)
     if MODE == "prod":
         download_schwab_token()
     await load_models()
+    task = asyncio.create_task(run_equity_level_one_stream())
     yield
+    task.cancel()
+    await task
 
 
 app = FastAPI(title="Axiom", lifespan=lifespan)
@@ -57,34 +59,6 @@ app.include_router(auth_router)
 app.include_router(equity_router)
 app.include_router(ml_router)
 app.include_router(stream_router)
-
-
-@app.post("/start")
-async def start_stream():
-    from axiom.ws.equity_level_one import run_equity_level_one_stream
-
-    global stream_task
-    if stream_task is None or stream_task.done():
-        stream_task = asyncio.create_task(run_equity_level_one_stream())
-        return {"message": "Stream started"}
-    else:
-        raise HTTPException(status_code=400, detail="Stream is already running")
-
-
-@app.post("/stop")
-async def stop_stream():
-    global stream_task
-    if stream_task is not None and not stream_task.done():
-        # Attempt to cancel the task
-        stream_task.cancel()
-        try:
-            await stream_task
-            return {"message": "Stream stopped"}
-        except asyncio.CancelledError:
-            return {"message": "Stream stopped successfully"}
-    else:
-        raise HTTPException(status_code=400, detail="Stream is not running")
-
 
 if __name__ == "__main__":
     import uvicorn
