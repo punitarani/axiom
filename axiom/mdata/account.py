3cd0190a023a3699297afe1d9ecf813659e3730a
Punit Arani <punitsai36@gmail.com>
2024-09-20 17:59:33 +0000

    Add get_account_hash(), get_account_info(), and get_account_transactions() functions

---
+++ axiom/mdata/account.py
@@ -0,0 +1,60 @@
+"""axiom/mdata/account.py"""
+
+from axiom.schwab_client import get_schwab_client, sch_limiter
+from axiom.schwab_models_account import Account, AccountNumberHash, Transaction
+from axiom.store.cache import account_info_cache
+
+# Hardcode the account idx to only support one account for now
+ACCOUNT_IDX = 0
+
+# Cache the account number hash as it doesn't change
+account_number_hash: AccountNumberHash | None = None
+
+
+async def get_account_hash() -> AccountNumberHash:
+    """Get the account hash for the user."""
+    global account_number_hash
+
+    if account_number_hash is None:
+        sch = get_schwab_client()
+
+        async with sch_limiter:
+            response = await sch.get_account_numbers()
+            account_info = response.json()[ACCOUNT_IDX]
+            account_number_hash = AccountNumberHash.model_validate(account_info)
+
+    return account_number_hash
+
+
+async def get_account_info() -> Account:
+    """Get accounts information for the user."""
+
+    # Check the cache first
+    cached_account_data = account_info_cache.get(ACCOUNT_IDX)
+    if cached_account_data is not None:
+        return cached_account_data
+
+    sch = get_schwab_client()
+
+    async with sch_limiter:
+        response = await sch.get_accounts()
+        account_info = response.json()[ACCOUNT_IDX]
+        account_data = Account.model_validate(account_info)
+
+        # Cache the account info for 5 minutes
+        account_info_cache.set(ACCOUNT_IDX, account_data, expire=300)
+
+        return account_data
+
+
+async def get_account_transactions() -> list[Transaction]:
+    """Get the transactions for a given account."""
+    sch = get_schwab_client()
+
+    async with sch_limiter:
+        account_number_hash = await get_account_hash()
+        response = await sch.get_transactions(account_number_hash.hashValue)
+
+        # Parse the Transaction objects
+        transactions = [Transaction.model_validate(transaction) for transaction in response.json()]
+        return transactions
--- axiom/store/cache.py
+++ axiom/store/cache.py
@@ -9,3 +9,6 @@ CACHE_DIR = DATA_DIR.joinpath("cache")
 level_one_cache = diskcache.Cache(directory=CACHE_DIR.joinpath("level_one"))
 daily_price_history_cache = diskcache.Cache(directory=CACHE_DIR.joinpath("daily_price_history"))
 instrument_cache = diskcache.Cache(directory=CACHE_DIR.joinpath("instrument"))
+
+# Account caches
+account_info_cache = diskcache.Cache(directory=CACHE_DIR.joinpath("account_info"))
