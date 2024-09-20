40e095b51767958cb2ad80c8fc91f09cda38e84f
Punit Arani <punitsai36@gmail.com>
2024-09-20 17:52:29 +0000

    Update generate_schwab_models.sh script to generate Account models

--- scripts/generate_schwab_models.sh
+++ scripts/generate_schwab_models.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-datamodel-codegen  --input ./data/schwab-openapi.json --input-file-type openapi --output ./axiom/schwab/models.py --output-model-type pydantic_v2.BaseModel
+datamodel-codegen  --input ./data/schwab-market-openapi.json --input-file-type openapi --output ./axiom/schwab_models_market.py --output-model-type pydantic_v2.BaseModel
+datamodel-codegen  --input ./data/schwab-account-openapi.json --input-file-type openapi --output ./axiom/schwab_models_account.py --output-model-type pydantic_v2.BaseModel
